03ac0ab35dd7386e5f0cecef1bed7e67080510cc
Allen Ravitsky <arr8@cryptolab.net>
2015-02-06 18:49:20 -0500

    Remove code from fork and tighten licensing. Fixes #4

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,7 @@
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses portions of this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
@@ -43,6 +43,7 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+	$( '#setup' ).show( 'fast' ); // Change from fork
 
         console.log('Received Event: ' + id);
     }
